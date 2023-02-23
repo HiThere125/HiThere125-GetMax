4c1b1329497a608faea6233aba28e6f1758b4fc1
Brent Kimura <bkimura73@gmail.com>
2023-02-23 07:52:47 +0000

    Completed Array Max Challenge

--- src/main/java/GetMax.java
+++ src/main/java/GetMax.java
@@ -7,6 +7,12 @@ public class GetMax {
      * @return the largest value in arr.
      */
     public int max(int[] arr){
-        return -1;
+        int GetMax = arr[0];
+        for(int i=0; i<arr.length; i++){
+            if(arr[i] > GetMax){
+                GetMax = arr[i];
+            }
+        }
+        return GetMax;
     }
 }
